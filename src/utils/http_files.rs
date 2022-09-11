1d70c6f20c4a3cae1ba471c98db18eaad0b15608
Carles Munyoz <cmunyoz@soltecsis.com>
2022-09-11 16:23:00 +0200

    Bug fix

--- src/utils/http_files.rs
+++ src/utils/http_files.rs
@@ -104,16 +104,7 @@ impl HttpFiles {
         self.move_tmp_files()?;
 
         // Install de FWCloud script.
-        let mut res: HttpResponse;
-        if self.ws_id != Uuid::nil() {
-            let ws_map = cfg.ws_map.lock().unwrap();
-            let ws_data = ws_map
-                .get(&self.ws_id)
-                .ok_or(FwcError::WebSocketIdNotFound)?;
-            res = run_cmd_ws("sh", &[&self.files[0].dst_path[..], "install"], ws_data)?;
-        } else {
-            res = run_cmd("sh", &[&self.files[0].dst_path[..], "install"])?;
-        }
+        let mut res = run_cmd("sh", &[&self.files[0].dst_path[..], "install"])?;
 
         // Load policy.
         for file in cfg.fwcloud_script_paths.iter() {
